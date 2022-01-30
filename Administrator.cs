9e1aee55067acb46d30443b08ef1b5b9183d70b0
Nikita Rimitsen <nikita.rimitsen@outlook.com>
2022-01-30 17:48:19 +0200

    kustuta, lisamine, uuendamine

--- Administrator.cs
+++ Administrator.cs
@@ -13,14 +13,16 @@ namespace Kinoteatr_bilet
     public partial class Administrator : Form
     {
 
-        static string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\opilane\source\repos\link_TARpv21\ProverkaZnPoMat\Kino_\AppData\Kino_DB.mdf;Integrated Security=True";
+        static string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\nikit\source\repos\kinnoooo\AppData\Kino_DB.mdf;Integrated Security=True";
         /*Надо менять            ↑ ↑ ↑ ↑ ↑ ↑ ↑  вот это, если ты пересел за другой комп!!!!!!!!!*/
         SqlConnection connect_to_DB = new SqlConnection(conn);
 
         SqlCommand command;
         SqlDataAdapter adapter;
 
-
+        Button film_uuenda;
+        Button film_kustuta;
+        Button film_insert;
         int Id_film;
         public Administrator()
         {
@@ -30,7 +32,7 @@ namespace Kinoteatr_bilet
             Button naita = new Button
             {
                 Text = "Naita",
-                Location = new System.Drawing.Point(500, 25),//Point(x,y)
+                Location = new System.Drawing.Point(40, 25),//Point(x,y)
                 Height = 40,
                 Width = 100,
                 BackColor = Color.LightYellow
@@ -38,16 +40,38 @@ namespace Kinoteatr_bilet
             naita.Click += Film_naita_Click;
 
 
-            Button film_uuenda = new Button
+            film_uuenda = new Button
             {
-                Text = "Naita",
-                Location = new System.Drawing.Point(250, 50),//Point(x,y)
-                Height = 40,
-                Width = 100,
-                BackColor = Color.LightYellow
+                Location = new System.Drawing.Point(600, 75),
+                Size = new System.Drawing.Size(80, 25),
+                Text = "Uuendamine",
+                Visible = false
             };
             film_uuenda.Click += Film_uuenda_Click;
 
+            film_kustuta = new Button
+            {
+                Location = new System.Drawing.Point(600, 100),
+                Size = new System.Drawing.Size(80, 25),
+                Text = "Kustuta",
+                Visible = false
+            };
+            film_kustuta.Click += Film_kustuta_Click;
+
+
+            film_insert = new Button
+            {
+                Location = new System.Drawing.Point(600, 125),
+                Size = new System.Drawing.Size(80, 25),
+                Text = "Lisa uus",
+                Visible = false
+            };
+            film_insert.Click += Film_insert_Click;
+
+
+            this.Controls.Add(film_insert);
+            this.Controls.Add(film_uuenda);
+            this.Controls.Add(film_kustuta);
             this.Controls.Add(naita);
             /*TextBox nimi = new TextBox
             {
@@ -64,6 +88,56 @@ namespace Kinoteatr_bilet
             };
             this.Controls.Add(nimi);
             this.Controls.Add(film);*/
+        }
+
+        private void Film_insert_Click(object sender, EventArgs e)
+        {
+            if (film_txt.Text != "" && aasta_txt.Text != "" && poster_txt.Text != "")
+            {
+                try
+                {
+                    command = new SqlCommand("INSERT INTO Film(Nimi,Aasta,Pilt) VALUES(@nimi,@aasta,@pilt)", connect_to_DB);
+                    connect_to_DB.Open();
+                    command.Parameters.AddWithValue("@nimi", film_txt.Text);
+                    command.Parameters.AddWithValue("@aasta", aasta_txt.Text);
+                    string file_pilt = film_txt.Text + ".jpg";
+                    command.Parameters.AddWithValue("@pilt", file_pilt);
+
+                    command.ExecuteNonQuery();
+                    connect_to_DB.Close();
+                    ClearData();
+                    DisplayData();
+                    MessageBox.Show("Andmed on lisatud");
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Viga lisamisega");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Viga else");
+            }
+        }
+
+        private void Film_kustuta_Click(object sender, EventArgs e)
+        {
+            if (Id_film != 0)
+            {
+                command = new SqlCommand("DELETE Film WHERE Id_film=@id", connect_to_DB);
+                connect_to_DB.Open();
+                command.Parameters.AddWithValue("@id", Id_film);
+                command.ExecuteNonQuery();
+                connect_to_DB.Close();
+                ClearData();
+                DisplayData();
+                MessageBox.Show("Andmed on kustutatud");
+            }
+            else
+            {
+                MessageBox.Show("Viga kustutamisega");
+            }
         }
 
         TextBox film_txt, aasta_txt, poster_txt;
@@ -84,9 +158,10 @@ namespace Kinoteatr_bilet
                 //command.Parameters.AddWithValue("@pilt", file_pilt);
                 command.ExecuteNonQuery();
                 connect_to_DB.Close();
-                //ClearData();
-                //Data();
+                ClearData();
+                Data();
                 MessageBox.Show("Andmed uuendatud");
+
             }
             else
             {
@@ -97,6 +172,9 @@ namespace Kinoteatr_bilet
 
         private void Film_naita_Click(object sender, EventArgs e)
         {
+            film_insert.Visible = true;
+            film_uuenda.Visible = true;
+            film_kustuta.Visible = true;
             film_txt = new TextBox
             {
                 Location = new System.Drawing.Point(450, 75),
@@ -140,7 +218,7 @@ namespace Kinoteatr_bilet
             DataTable tabel = new DataTable();
             dataGridView = new DataGridView();
             dataGridView.RowHeaderMouseClick += DataGridView_RowHeaderMouseClick;
-            SqlDataAdapter adapter = new SqlDataAdapter("SELECT Id_film,Nimi,Aasta,Pilt FROM [dbo].[Film]", connect_to_DB);
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [dbo].[Film]", connect_to_DB);
             adapter.Fill(tabel);
             dataGridView.DataSource = tabel;
             dataGridView.Location = new System.Drawing.Point(10, 75);
@@ -156,6 +234,28 @@ namespace Kinoteatr_bilet
             poster.Image = Image.FromFile(@"C:..\..\Posterid\" + dataGridView.Rows[e.RowIndex].Cells[3].Value.ToString());
             //string v = dataGridView.Rows[e.RowIndex].Cells[5].Value.ToString();
             //comboBox1.SelectedIndex = Int32.Parse(v) - 1;
+        }
+
+        private void DisplayData()
+        {
+            connect_to_DB.Open();
+            DataTable tabel = new DataTable();
+            adapter = new SqlDataAdapter("SELECT * FROM Film", connect_to_DB);//, Kategooria WHERE Toodetable.Kategooria_Id=Kategooria.Id
+            adapter.Fill(tabel);
+            dataGridView.DataSource = tabel;
+            connect_to_DB.Close();
+
+        }
+
+        private void ClearData()
+        {
+            //Id = 0;
+            film_txt.Text = "";
+            aasta_txt.Text = "";
+            poster_txt.Text = "";
+            //save.FileName = "";
+            poster.Image = Image.FromFile("../../Posterid/ezik.jpg");
+
         }
     }
 }
